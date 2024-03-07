ec4a31ca876e4f94998052489f7384931c005f10
safvanmc <safwanmc81@gmail.com>
2024-03-08 01:39:23 +0530

    saf commit

--- lib/modals/expenceitem.dart
+++
@@ -1,8 +0,0 @@
-class ExpenseItem {
-  final String name;
-  final String amount;
-  final DateTime datetime;
-
-  ExpenseItem(
-      {required this.name, required this.amount, required this.datetime});
-}
